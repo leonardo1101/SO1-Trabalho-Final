5eae6fde4bc6924f417a23230607ca8ab4d71d65
leonardo1101 <leonardo98ti@gmail.com>
2018-07-13 09:29:51 -0300

    Comentarios taks 4

--- xv6/trap.c
+++ xv6/trap.c
@@ -45,10 +45,10 @@ trap(struct trapframe *tf)
     exit();
     return;
   }
-  // COW: Caso haja um pagefault
+  // Caso haja um pagefault
   if (tf->trapno == T_PGFLT) {
     proc->tf = tf;
-    // Lida com a questão de tratar o pagefault ocorrido
+    // Tratar o pagefault ocorrido
     handle_pgflt();
     return;
   }
